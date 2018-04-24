77f3b0b8e4ac4cfe619ebe6e70a349512bb42f2e
ervinbani <bani.ervin@gmail.com>
2018-04-24 15:45:28 +0200

    main

--- main.js
+++ main.js
@@ -1,11 +1,16 @@
 $(document).ready(function(){
 
+
     $(document).on('click', '#btn', function(){
         var inputUtente=$('.container').children('.input').val();
-
-        $.ajax(function(){
-            url:'https://api.themoviedb.org/3/movie/550?api_key=936b60fb6691f67d5eda3a0a29507da1',
+        $.ajax({
+            url:'https://api.themoviedb.org/search/movie',
             method:'GET',
+            data:{
+                api_key:'936b60fb6691f67d5eda3a0a29507da1',
+                query:inputUtente,
+                language:'IT'
+            },
             succes: function(data){
                 console.log(data);
             },
@@ -13,6 +18,8 @@ $(document).ready(function(){
                 alert('errore');
             }
         });//fine ajax
+
+
 
 
     });//fine del document.on
