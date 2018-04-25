c52b6f617e435e2ac9276e3a9343ed6167ce32e0
ervinbani <bani.ervin@gmail.com>
2018-04-25 20:15:06 +0200

    intitial m2

--- main.js
+++ main.js
@@ -1,13 +1,22 @@
+var dataContainer=$('.container').children('.data-Container');
+var inputUtente=$('.container').children('.input').val();
+var arrFlags=['flags/it.PNG', 'flags/en.PNG', 'flags/es.PNG'];
+
+
+
 $(document).ready(function(){
-    var dataContainer=$('.container').children('.data-Container');
-    var inputUtente=$('.container').children('.input').val();
+
+
+    var arr=0;
+
 
     $(document).on('click', $('#btn'), function(){
         searchMovies();
+
+
     });//fine del document.on
 
     function searchMovies(){
-      var arr=[];
       $.ajax({
           url:'https://api.themoviedb.org/3/search/movie?',
           method:'GET',
@@ -17,32 +26,43 @@ $(document).ready(function(){
               language:'it-IT'
           },
           success: function(data){
-              risultato=data.results;
-              console.log(risultato);
+              var risultato=data.results;
               for(var i=0;i<risultato.length;i++){
-              console.log('tittle', risultato[i]['original_title']);
-              dataContainer.append('Risultato nr', (i+1));
-              dataContainer.append('<p>'+'Titolo:-'+risultato[i]['title']+'</p>');
-              dataContainer.append('<p>'+'Titolo originale:-'+risultato[i]['original_title']+'</p>');
-              dataContainer.append('<p>'+'Lingua originale:-'+risultato[i]['original_language']+'</p>');
-              dataContainer.append('<p>'+'Voto:='+transformAverage(risultato[i]['vote_average'])+'<br>'+'<br>'+'</p>');
+                  console.log('tittle', risultato[i]['original_title']);
+                  dataContainer.append('Risultato nr', (i+1));
+                  dataContainer.append('<p>'+'Titolo:-'+risultato[i]['title']+'</p>');
+                  dataContainer.append('<p>'+'Titolo originale:-'+risultato[i]['original_title']+'</p>');
+                  dataContainer.append('<p class="leng">'+'Lingua originale:-'+risultato[i]['original_language']+'<img id='+i+' src=fonte style="height:20px">'+'</p>');
+                  dataContainer.append('<p>'+'Voto:='+transformAverage(risultato[i]['vote_average'])+'<br><br>'+'</p>');
+                  for(var j=0;j<arrFlags.length;j++){
+                      if(arrFlags[j]==('flags/'+(risultato[i]['original_language'])+'.'+'svg')){
+                            var fonte='flags/'+arrFlags[j]+'.'+'svg';
+                            dataContainer.children('.leng').children('img').attr('src', fonte);
 
+                      }
+
+
+                  }
               }
-
-          },
+              //generateFlag();
+              console.log('risultato essatto', risultato);
+            },
           error:function(){
               alert('errore');
           }
       });//fine ajax
 
+
     }
+    //funzione che transforma la media in un numero x da 1 a 5 e stampa x star invece del num x
     function transformAverage(vote_average){
         starVote=Math.floor(vote_average/2);
         var arrStar=[];
         for(var i=0;i<starVote;i++){
-            arrStar.push('<i class="fas fa-star"></i>');
+            arrStar.push('<i class="fas fa-star" style="color:yellow"></i>');
         }
         return arrStar;
     }
+
 
 });//fine del doc ready
